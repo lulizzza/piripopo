8542e5712ed9e73775df01f850e561f79ee7b890
lulizzza <142451759+lulizzza@users.noreply.github.com>
2024-09-25 08:18:08 -0300

    dfg

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ console.log(soma(20,30));
 function areaQuadrado(lado) {
     return lado * lado
 }
+
+let media = function(num1,num2,num3){
+    return (num1,num2,num3) /3
+}
+
+console.log(media(4,6,8))
