0440ec8f6bff5f2c920fae89ecd27b37cba34297
lulizzza <142451759+lulizzza@users.noreply.github.com>
2024-09-25 08:47:02 -0300

    dfbnm,

--- main.js
+++ main.js
@@ -8,8 +8,13 @@ function areaQuadrado(lado) {
     return lado * lado
 }
 
+
 let media = function(num1,num2,num3){
     return (num1+num2+num3) /3
 }
+let num1 = parseInt(prompt("insira o primeiro numero"))
+let num2 = parseInt(prompt("insira o segundo numero"))
+let num3 = parseInt(prompt("insira o terveiro numero"))
+document.write(media(num1, num2, num3))
 
 console.log(media(4,6,8))
