e3bcc6583f3fc067e27a13f95544ec5a084dacf7
lulizzza <142451759+lulizzza@users.noreply.github.com>
2024-10-02 07:56:06 -0300

    sdfghj

--- main.js
+++ main.js
@@ -22,3 +22,9 @@
 let estados=[
 "AC","AL","AM","AP","BA","CE","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO","DF"
 ];
+
+let i=0;
+while(i<5){
+console.log(i);
+i=i+1;
+}
