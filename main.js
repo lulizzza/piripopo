bcb8dc43b1cd496db5d4c8af5a0f55d5dc1286f6
lulizzza <142451759+lulizzza@users.noreply.github.com>
2024-10-02 07:39:16 -0300

    FGHJM,.

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
-function soma(num1,num2){
-    return num1+num2
-}
+// function soma(num1,num2){
+//     return num1+num2
+// }
 
-console.log(soma(20,30));
+// console.log(soma(20,30));
 
-function areaQuadrado(lado) {
-    return lado * lado
-}
+// function areaQuadrado(lado) {
+//     return lado * lado
+// }
 
 
-let media = function(num1,num2,num3){
-    return (num1+num2+num3) /3
-}
-let num1 = parseInt(prompt("insira o primeiro numero"))
-let num2 = parseInt(prompt("insira o segundo numero"))
-let num3 = parseInt(prompt("insira o terceiro numero"))
-document.write(media(num1, num2, num3))
+// let media = function(num1,num2,num3){
+//     return (num1+num2+num3) /3
+// }
+// let num1 = parseInt(prompt("insira o primeiro numero"))
+// let num2 = parseInt(prompt("insira o segundo numero"))
+// let num3 = parseInt(prompt("insira o terceiro numero"))
+// document.write(media(num1, num2, num3))
 
-console.log(media(4,6,8))
+// console.log(media(4,6,8))
+
+let estados=[
+"PR","RS","AC","BA","SC","SP","RJ","MG","MT","MS","ES","RR","RO","PA","CE","SE","PB","RN","PI","TO","AL","MA","PE","GO","AP","AM","DF"
+];
